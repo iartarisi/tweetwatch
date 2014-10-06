2b50ddf161e026a70b2495d5a04c6f3d6ab40298
Ionuț Arțăriși <ionut@artarisi.eu>
2014-10-07 01:01:02 +0200

    query the database for the most recent and retweeted

--- lib/eventsource.rb
+++ lib/eventsource.rb
@@ -1,14 +1,18 @@
 require 'faye/websocket'
 
+require 'schema'
 require 'tweetwatch'
+
+LAST_N = 10
+LAST_X_MINUTES = 10
+
 
 App = lambda do |env|
   if Faye::EventSource.eventsource?(env)
     es = Faye::EventSource.new(env)
 
     loop = EM.add_periodic_timer(1) do
-      ts = Tweet.order(created_at: :desc).limit(10).map {|t| [t.id, t.text]}
-      es.send(ts.to_json)
+      es.send(bestof.to_json)
     end
 
     es.on :close do |event|
@@ -23,4 +27,18 @@ App = lambda do |env|
     # Normal HTTP request
     [200, {'Content-Type' => 'text/html,application'}, [IO.read('static/index.html')]]
   end
+end
+
+
+def bestof
+  tweets = Tweet.find_by_sql(<<-SQL)
+    SELECT DISTINCT tweet_id, text,
+       COUNT(created_at) OVER (PARTITION BY tweet_id) AS retweeted
+    FROM tweets
+    WHERE AGE(NOW() AT TIME ZONE 'UTC', created_at)
+          < INTERVAL '#{LAST_X_MINUTES} MINUTES'
+    ORDER BY retweeted DESC
+    LIMIT #{LAST_N}
+SQL
+  tweets.map { |t| { id: t.tweet_id, text: t.text, retweeted: t.retweeted } }
 end
---
+++ spec/eventsource_spec.rb
@@ -0,0 +1,53 @@
+require 'eventsource'
+require 'schema'
+
+require_relative 'spec_helper'
+
+describe '#bestof' do
+  before { Schema.migrate(:setup) }
+  after { Schema.migrate(:teardown) }
+
+  subject { bestof }
+
+  it 'returns the most retweeted N items in the last N minutes' do
+    # ignores one tweet that is too old
+    Tweet.create(tweet_id: 0, text: 'old',
+      created_at: Time.now.utc - LAST_X_MINUTES * 60 - 1)
+
+    Tweet.create(tweet_id: 1, text: 'foo', created_at: Time.now.utc)
+    Tweet.create(tweet_id: 1, text: 'foo', created_at: Time.now.utc)
+    Tweet.create(tweet_id: 2, text: 'qux', created_at: Time.now.utc)
+    Tweet.create(tweet_id: 3, text: 'bar', created_at: Time.now.utc)
+    Tweet.create(tweet_id: 3, text: 'bar', created_at: Time.now.utc)
+    Tweet.create(tweet_id: 3, text: 'bar', created_at: Time.now.utc)
+
+    Tweet.create(tweet_id: 4, text: 'baz', created_at: Time.now.utc)
+    Tweet.create(tweet_id: 5, text: 'foo', created_at: Time.now.utc)
+
+    # ignores retweets that are too old
+    Tweet.create(tweet_id: 6, text: 'old',
+      created_at: Time.now.utc - LAST_X_MINUTES * 60 - 1)
+    Tweet.create(tweet_id: 6, text: 'old',
+      created_at: Time.now.utc - LAST_X_MINUTES * 60 - 1)
+
+    Tweet.create(tweet_id: 7, text: 'baz', created_at: Time.now.utc)
+    Tweet.create(tweet_id: 8, text: 'baz', created_at: Time.now.utc)
+    Tweet.create(tweet_id: 9, text: 'baz', created_at: Time.now.utc)
+    Tweet.create(tweet_id: 10, text: 'baz', created_at: Time.now.utc)
+    Tweet.create(tweet_id: 11, text: 'baz', created_at: Time.now.utc)
+
+    expect(subject.length).to eq(LAST_N)
+    is_expected.to eq [
+      {id: 3, text: 'bar', retweeted: 3},
+      {id: 1, text: 'foo', retweeted: 2},
+      {id: 2, text: 'qux', retweeted: 1},
+      {id: 4, text: 'baz', retweeted: 1},
+      {id: 5, text: 'foo', retweeted: 1},
+      {id: 7, text: 'baz', retweeted: 1},
+      {id: 8, text: 'baz', retweeted: 1},
+      {id: 9, text: 'baz', retweeted: 1},
+      {id: 10, text: 'baz', retweeted: 1},
+      {id: 11, text: 'baz', retweeted: 1}
+    ]
+  end
+end
