ec92bf9206f1438895a5edfc9725f432822748a4
Ionuț Arțăriși <iartarisi@suse.cz>
2014-10-06 17:54:51 +0200

    refactor tests to use let for the shared object

--- spec/tweetwatch_spec.rb
+++ spec/tweetwatch_spec.rb
@@ -1,18 +1,15 @@
 require 'tweetwatch'
 require 'schema'
 
-ActiveRecord::Base.establish_connection :test
+require_relative 'spec_helper'
 
 describe '#handle_tweets' do
   before{ Schema.migrate(:setup) }
-
   after { Schema.migrate(:teardown) }
+  let (:status) { instance_double("Twitter::Tweet", text:"Twitter is awesome!",
+      id: 123, created_at: Time.now.utc, retweeted_status?: false) }
 
   it 'creates a tweet when it receives a new status' do
-    status = instance_double("Twitter::Tweet", text:"Twitter is awesome!",
-      id: 123, created_at: Time.now.utc)
-
-    expect(status).to receive(:retweeted_status?).and_return(false)
     handle_tweets status
 
     tweet = Tweet.first
@@ -22,18 +19,16 @@ describe '#handle_tweets' do
   end
 
   it 'creates a tweet with the original status' do
-    original = instance_double("Twitter::Tweet", text: "original",
-      id: 123, created_at: Time.now.utc)
-    status = instance_double("Twitter::Tweet", text:"not the original")
+    retweet = instance_double("Twitter::Tweet", text:"not the original")
 
-    expect(status).to receive(:retweeted_status?).and_return(true)
-    expect(status).to receive(:retweeted_status).and_return original
-    handle_tweets status
+    expect(retweet).to receive(:retweeted_status?).and_return(true)
+    expect(retweet).to receive(:retweeted_status).and_return status
+    handle_tweets retweet
 
     expect(Tweet.count).to eq 1
     tweet = Tweet.first
-    expect(tweet.id).to eq original.id
-    expect(tweet.text).to eq original.text
-    expect(tweet.created_at).to be_within(0.000001).of original.created_at
+    expect(tweet.id).to eq status.id
+    expect(tweet.text).to eq status.text
+    expect(tweet.created_at).to be_within(0.000001).of status.created_at
   end
 end
