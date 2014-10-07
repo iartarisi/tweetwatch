cb2e9d669790ff99c0546ac643aa645b1dc1b986
Ionuț Arțăriși <iartarisi@suse.cz>
2014-10-07 11:17:18 +0200

    use a 64bit int to store tweet ids

--- lib/schema.rb
+++ lib/schema.rb
@@ -6,7 +6,7 @@ ActiveRecord::Base.configurations = YAML::load(IO.read('database.yml'))
 class Schema < ActiveRecord::Migration
   def setup
     create_table :tweets do |t|
-      t.integer :tweet_id, null: false
+      t.integer :tweet_id, null: false, limit: 8  # 64bit int
       t.string :text
       t.datetime :created_at
       t.index :created_at
